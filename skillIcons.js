8744381ec7659b798ac6cc36009f2facae8992a7
Pushkar Mishra <55252939+p2k3m@users.noreply.github.com>
2025-09-07 11:03:51 +0530

    Auto-populate default skill icons and levels

---
+++ skillIcons.js
@@ -0,0 +1,12 @@
+export const DEFAULT_SKILL_ICONS = {
+  javascript: 'fa-brands fa-js',
+  python: 'fa-brands fa-python',
+  html: 'fa-brands fa-html5',
+  css: 'fa-brands fa-css3-alt',
+  node: 'fa-brands fa-node-js',
+  react: 'fa-brands fa-react',
+  docker: 'fa-brands fa-docker',
+  aws: 'fa-brands fa-aws'
+}
+
+export default DEFAULT_SKILL_ICONS
