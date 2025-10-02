756c058fe44f3d33afacabff112b16a0ef8d1597
Pushkar Mishra <55252939+p2k3m@users.noreply.github.com>
2025-10-02 16:42:24 +0530

    Add GetItemCommand mock for DynamoDB (#661)

--- tests/mocks/aws-sdk-client-dynamodb.js
+++ tests/mocks/aws-sdk-client-dynamodb.js
@@ -42,3 +42,10 @@ export class DeleteItemCommand {
     this.__type = 'DeleteItemCommand';
   }
 }
+
+export class GetItemCommand {
+  constructor(input) {
+    this.input = input;
+    this.__type = 'GetItemCommand';
+  }
+}
