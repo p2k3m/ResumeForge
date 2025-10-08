f50047df7404f78cb93043782f1875b456123500
Pushkar Mishra <55252939+p2k3m@users.noreply.github.com>
2025-10-08 07:02:14 +0530

    style: standardize typography across platforms

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,14 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-sans)', ...defaultTheme.fontFamily.sans],
+        mono: ['var(--font-mono)', ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
   plugins: [],
 }
