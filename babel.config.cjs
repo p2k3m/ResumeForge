aa2538523cb25c62f65a8515e53c0ccb8e42a904
Pushkar Mishra <55252939+p2k3m@users.noreply.github.com>
2025-10-30 06:47:06 +0530

    Fix upload hashed assets test and optional jest setup

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,4 +1,5 @@
 let presetEnv = null
+let presetReact = null
 
 try {
   const resolvedPreset = require('@babel/preset-env')
@@ -12,10 +13,19 @@ try {
   // gracefully continue without it.
 }
 
+try {
+  const resolvedPreset = require('@babel/preset-react')
+  presetReact = resolvedPreset?.default ?? resolvedPreset
+} catch (error) {
+  if (error.code !== 'MODULE_NOT_FOUND') {
+    throw error
+  }
+}
+
 module.exports = {
   presets: [
     presetEnv && [presetEnv, { targets: { node: 'current' } }],
-    ['@babel/preset-react', { runtime: 'automatic' }]
+    presetReact && [presetReact, { runtime: 'automatic' }],
   ].filter(Boolean),
-  plugins: ['@babel/plugin-syntax-import-meta']
+  plugins: ['@babel/plugin-syntax-import-meta'],
 }
